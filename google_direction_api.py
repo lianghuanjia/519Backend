8e0e3a3b424ae77e41b135ec81ad7455efdb995a
Huanjia Liang <huanjial@uci.edu>
2023-05-09 00:27:30 -0400

    Finished getting direction from one place to another

---
+++ google_direction_api.py
@@ -0,0 +1,57 @@
+import requests
+from credential import GOOGLE_MAP_API_KEY
+import re
+
+# Replace YOUR_API_KEY with your actual API key
+api_key = GOOGLE_MAP_API_KEY
+
+
+# ['Soldiers Field Road', '42.3645558', '-71.1359136']
+# ['655 Commonwealth Ave', '42.34993389999999', '-71.1027624']
+
+
+# Make an HTTP request to the Directions API
+
+def get_direction_from_one_place_to_another(one_place, another_place, traveling_mode):
+    """
+
+    :param one_place:
+        A string that contains latitude and longitude, with "," in the middle:
+        e.g.:'42.3645558,-71.1359136'
+    :param another_place:
+         A string that contains latitude and longitude, with "," in the middle:
+         e.g.: '42.34993389999999, -71.1027624'
+    :param traveling_mode: The traveling mode that the user prefers. E.g.: driving
+    :return: A list of all steps, each step is a string.
+    """
+    url = f"https://maps.googleapis.com/maps/api/directions/json?origin={origin}&destination={destination}&mode={mode}&key={api_key}"
+    response = requests.get(url)
+
+    # Parse the JSON response
+    result = response.json()
+
+    # Extract the steps from the first route
+    route = result["routes"][0]
+    legs = route["legs"]
+    steps = [step for leg in legs for step in leg["steps"]]
+
+    directions = []
+    for i, step in enumerate(steps):
+        step_str = f"Step {i + 1}: {step['html_instructions']}"
+        # get rid of <div ...> and </div>
+        deleted_div_step = re.sub(r'<div.*?>|</div>', '', step_str)
+        # get rid of <b> </b>
+        deleted_b_step = re.sub(r'<\/?b>', '', deleted_div_step)
+        directions.append(deleted_b_step)
+    return directions
+
+
+if __name__ == "__main__":
+    # Define the starting and ending addresses
+    origin = '42.3645558,-71.1359136'
+    destination = "42.3645558,-71.1359136"  # "42.34993389999999, -71.1027624"
+
+    # Define the mode of transportation
+    mode = "driving"
+    d = get_direction_from_one_place_to_another(origin, destination, mode)
+    print(d)
